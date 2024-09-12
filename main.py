c061ff0fe68e415409351b55889297b562e3d339
Maksim <knyazev.maksim2852@gmail.com>
2024-09-12 16:05:07 +0400

    add mount

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
-
-
+from app.api.v1.main import main_router
+from fastapi.staticfiles import StaticFiles
 
 
 app: FastAPI = FastAPI()
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+
+app.include_router(main_router)
