fbca3a0a3746228a46ef97d3e018d8d10bd407a7
Maksim <knyazev.maksim2852@gmail.com>
2024-09-13 17:21:08 +0400

    update js

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -1,3 +1,27 @@
 function triggerFileInput() {
     document.getElementById('file-input').click();
 }
+
+function uploadFile() {
+    const fileInput = document.getElementById('file-input');
+    const file = fileInput.files[0];
+
+    if (file) {
+        const formData = new FormData();
+        formData.append('file', file);
+
+        fetch('/upload', {
+            method: 'POST',
+            body: formData
+        }).then(response => {
+            if (response.ok) {
+                window.location.reload();
+            } else {
+                alert('Ошибка при загрузке файла');
+            }
+        }).catch(error => {
+            console.error('Ошибка:', error);
+            alert('Ошибка при отправке запроса');
+        });
+    }
+}
