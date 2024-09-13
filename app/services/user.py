d95582d9c79ad685afd10c955ba2a2e661b746bf
Maksim <knyazev.maksim2852@gmail.com>
2024-09-13 17:22:01 +0400

    add User services

---
+++ app/services/user.py
@@ -0,0 +1,25 @@
+from app.repositories.user import UserRepository
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+
+from app.repositories.base import AbstractRepository
+from app.schemas.user import UserOut
+
+
+class UserService:
+
+    def __init__(self, repository: type[UserRepository]) -> None:
+        self.repository= repository()
+
+    async def create(self, session: AsyncSession, ip_address: str) -> UserOut:
+        return await self.repository.add(session=session, ip_address=ip_address)
+
+    async def delete(self, id: int) -> int:
+        await self.repository.delete(id=id)
+
+    async def get_one(self, ip_address: str, session: AsyncSession) -> UserOut:
+        return await self.repository.find_one_or_none(ip_address=ip_address, session=session)
+ 
+
+
