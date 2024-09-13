30a2407029d3dc3584d1aa2323533a4abe09faf3
Maksim <knyazev.maksim2852@gmail.com>
2024-09-13 17:17:04 +0400

    add mount

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.api.v1.main import main_router
+from app.api.v1.upload import upload_router
 from fastapi.staticfiles import StaticFiles
 
 
@@ -9,3 +10,4 @@ app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 
 app.include_router(main_router)
+app.include_router(upload_router)
