fd0304958bd655099a86144c45d1a7a3a26b6f6b
Maksim <knyazev.maksim2852@gmail.com>
2024-09-13 21:58:11 +0400

    update js

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -5,23 +5,36 @@ function triggerFileInput() {
 function uploadFile() {
     const fileInput = document.getElementById('file-input');
     const file = fileInput.files[0];
+    const loadingMessage = document.getElementById('loading-message');
 
     if (file) {
         const formData = new FormData();
         formData.append('file', file);
+
+        loadingMessage.style.display = 'block';
 
         fetch('/upload', {
             method: 'POST',
             body: formData
         }).then(response => {
             if (response.ok) {
+                loadingMessage.style.display = 'none';
                 window.location.reload();
             } else {
                 alert('Ошибка при загрузке файла');
+                loadingMessage.style.display = 'none';
             }
         }).catch(error => {
             console.error('Ошибка:', error);
             alert('Ошибка при отправке запроса');
+            loadingMessage.style.display = 'none';
         });
     }
+}
+function copyToClipboard(text) {
+    navigator.clipboard.writeText(text).then(function() {
+        alert("Ссылка скопирована в буфер обмена");
+    }, function(err) {
+        console.error('Ошибка копирования', err);
+    });
 }
