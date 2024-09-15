1fc2ec9b92047d8ea8c8fbb0f9e73d3a969baa19
Maksim <knyazev.maksim2852@gmail.com>
2024-09-15 18:34:49 +0400

    update file services

--- app/services/file.py
+++ app/services/file.py
@@ -13,11 +13,11 @@ class FileService:
     async def create(self, session: AsyncSession, **data) -> FileSchema:
         return await self.repository.add(session=session, **data)
 
-    async def delete(self, id: int) -> int:
-        await self.repository.delete(id=id)
+    async def delete(self, id: int, session: AsyncSession) -> int:
+        await self.repository.delete(id=id, session=session)
 
-    async def get_one(self, ip_address: str, session: AsyncSession) -> FileSchema:
-        return await self.repository.find_one_or_none(ip_address=ip_address, session=session)
+    async def get_one(self, session: AsyncSession, **filter_by) -> FileSchema:
+        return await self.repository.find_one_or_none(session=session, **filter_by)
 
     async def get_all(self, session: AsyncSession, **data) -> list[FileSchema]:
         return await self.repository.find_all(session=session, **data)
