500f0a5919f3f26fe7a99fef05a5e68cda172835
Maksim <knyazev.maksim2852@gmail.com>
2024-09-15 19:57:52 +0400

    add pagination

--- app/api/v1/main.py
+++ app/api/v1/main.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, Request, Depends
+from fastapi import APIRouter, Request, Depends, Path, BackgroundTasks, Query
+from fastapi.responses import HTMLResponse, RedirectResponse, StreamingResponse
+from app.schemas.file import FileSchema
 from app.schemas.user import UserOut
 from app.utils.templating import get_template
 from fastapi.templating import Jinja2Templates
@@ -8,11 +10,17 @@ from app.services.user import UserService
 from database import get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import Annotated
+from app.utils.S3 import s3_client
+from app.tasks.tasks import delete_file_from_s3
+from exceptions import NotAccessException, UserNotFoundException
+from config import settings
 
 
 
 main_router: APIRouter = APIRouter(tags=['Главная страница'])
 
+
+TYPE_CONTENT: list[str] = ['video', 'audio', 'image']
 
 @main_router.get('/')
 async def get_main_page(
@@ -20,14 +28,87 @@ async def get_main_page(
         template: Annotated[Jinja2Templates, Depends(get_template)],
         user_service: Annotated[UserService, Depends(get_user_service)],
         file_service: Annotated[FileService, Depends(get_file_service)],
-        session: Annotated[AsyncSession, Depends(get_async_session)]
+        session: Annotated[AsyncSession, Depends(get_async_session)],
+        page: Annotated[int | None, Query()] = 1,
+    ):
+    offset = (page - 1) * 5
+    ip_address: str = request.client.host
+    user: UserOut = await user_service.get_one(ip_address=ip_address, session=session)
+
+    if user:
+        user_files, total_count = await file_service.get_all(session=session, user_id=user.id, offset=offset)
+        total_pages = (total_count + 5 - 1) // 5
+        return template.TemplateResponse(
+            request=request, 
+            name='base.html', 
+            context={'user_files': user_files, 'current_page': page, 'total_pages': total_pages, 'min': min})
+    
+    return template.TemplateResponse(request=request, name='base.html', context={'user_files': []})
+
+
+   
+
+@main_router.get('/{file_url}')
+async def get_main_page(
+        request: Request, 
+        file_url: Annotated[str, Path()],
+        template: Annotated[Jinja2Templates, Depends(get_template)],
+        file_service: Annotated[FileService, Depends(get_file_service)],
+        session: Annotated[AsyncSession, Depends(get_async_session)],
+    ):
+
+    file: FileSchema = await file_service.get_one(url=file_url, session=session)
+    if file:
+        file_content: str = file.content_type.split('/')[0]
+        if file_content in TYPE_CONTENT:
+            path: str = f'{settings.url}/{file.url}_{file.filename}'
+            return template.TemplateResponse(request=request, name='file.html', context={
+                'file': file,
+                'file_url': path,
+            })
+        return RedirectResponse(f'{settings.url}/{file.url}_{file.filename}')
+    return template.TemplateResponse(request=request, name='404.html')
+        
+
+
+@main_router.delete('/{file_id}')
+async def delete_file(
+        request: Request,
+        file_id: Annotated[int, Path()],
+        user_service: Annotated[UserService, Depends(get_user_service)],
+        file_service: Annotated[FileService, Depends(get_file_service)],
+        session: Annotated[AsyncSession, Depends(get_async_session)],
+        bg_task: BackgroundTasks
     ):
 
     ip_address: str = request.client.host
     user: UserOut = await user_service.get_one(ip_address=ip_address, session=session)
 
-    if user:
-        list_files: list[FileService] = await file_service.get_all(session=session, user_id=user.id)
-        return template.TemplateResponse(request=request, name='base.html', context={'user_files': list_files})
+    if not user:
+        raise UserNotFoundException
     
-    return template.TemplateResponse(request=request, name='base.html', context={'user_files': []})
+    file: FileSchema = await file_service.get_one(id=file_id, session=session)
+
+    if not file or file.user_id != user.id:
+        raise NotAccessException
+    
+    await file_service.delete(id=file_id, session=session)
+
+    filename: str = f'{file.url}_{file.filename}'
+    bg_task.add_task(
+        delete_file_from_s3,
+        filename
+    )
+    
+
+    
+@main_router.post('/report/{file_id}')
+async def report_file(
+        file_id: Annotated[int, Path()],
+        file_service: Annotated[FileService, Depends(get_file_service)],
+        session: Annotated[AsyncSession, Depends(get_async_session)],
+        bg_task: BackgroundTasks
+    ):
+
+    file: FileSchema = await file_service.get_one(session=session, id=file_id)
+    print(file)
