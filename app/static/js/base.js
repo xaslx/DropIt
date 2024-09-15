e2cb03b428de6dc4430ad9ac6ae7b05848c42171
Maksim <knyazev.maksim2852@gmail.com>
2024-09-15 11:57:36 +0400

    add notifications

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -7,6 +7,35 @@ function uploadFile() {
     const file = fileInput.files[0];
     const loadingMessage = document.getElementById('loading-message');
 
+    const notyf = new Notyf({
+        duration: 1500,
+        dismissible: true,
+        position: {
+            x: 'center',
+            y: 'top'
+        },
+        types: [
+            {
+                type: 'success',
+                background: '#8BC34A',
+                icon: {
+                    className: 'fas fa-check',
+                    tagName: 'i',
+                    text: ''
+                }
+            },
+            {
+                type: 'error',
+                background: 'red',
+                icon: {
+                    className: 'fas fa-exclamation-triangle',
+                    tagName: 'i',
+                    text: ''
+                }
+            }
+        ]
+    });
+
     if (file) {
         const formData = new FormData();
         formData.append('file', file);
@@ -17,20 +46,29 @@ function uploadFile() {
             method: 'POST',
             body: formData
         }).then(response => {
+            loadingMessage.style.display = 'none';
             if (response.ok) {
-                loadingMessage.style.display = 'none';
-                window.location.reload();
+       
+                notyf.success('Файл успешно загружен!');
+                setTimeout(() => {
+                    window.location.reload();
+                }, 1500);
+            } else if (response.status === 413) {
+             
+                notyf.error('Ошибка: Файл слишком большой.');
             } else {
-                alert('Ошибка при загрузке файла');
-                loadingMessage.style.display = 'none';
+              
+                notyf.error('Ошибка при загрузке файла.');
             }
         }).catch(error => {
             console.error('Ошибка:', error);
-            alert('Ошибка при отправке запроса');
             loadingMessage.style.display = 'none';
+            notyf.error('Ошибка при отправке запроса. Попробуйте позже.');
         });
     }
 }
+
+
 function copyToClipboard(text) {
     navigator.clipboard.writeText(text).then(function() {
         alert("Ссылка скопирована в буфер обмена");
@@ -38,3 +76,79 @@ function copyToClipboard(text) {
         console.error('Ошибка копирования', err);
     });
 }
+
+
+let fileIdToDelete = null;
+
+
+function openModal(fileId) {
+    fileIdToDelete = fileId;
+    document.getElementById("modal").style.display = "block";
+}
+
+function closeModal() {
+    fileIdToDelete = null;
+    document.getElementById("modal").style.display = "none";
+}
+
+
+function confirmDeletion() {
+    const notyf = new Notyf({
+        duration: 1500,
+        dismissible: true,
+        position: {
+            x: 'center',
+            y: 'top'
+        },
+        types: [
+            {
+                type: 'success',
+                background: '#8BC34A',
+                icon: {
+                    className: 'fas fa-check',
+                    tagName: 'i',
+                    text: ''
+                }
+            },
+            {
+                type: 'error',
+                background: 'red',
+                icon: {
+                    className: 'fas fa-exclamation-triangle',
+                    tagName: 'i',
+                    text: ''
+                }
+            }
+        ]
+    });
+    if (fileIdToDelete) {
+        fetch(`${fileIdToDelete}`, {
+            method: 'DELETE'
+        })
+        .then(response => {
+            if (response.ok) {
+       
+                notyf.success('Файл был успешно удален.');
+                closeModal();
+                setTimeout(() => {
+                    location.reload();
+                }, 1500);
+            } else {
+       
+                notyf.error('Ошибка при удалении файла.');
+            }
+        })
+        .catch(error => {
+            console.error("Произошла ошибка:", error);
+
+            notyf.error('Ошибка при отправке запроса на удаление. Попробуйте позже.');
+        });
+    }
+}
+
+document.querySelectorAll('.delete-button').forEach(button => {
+    button.addEventListener('click', function() {
+        const fileId = this.getAttribute('data-file-id');
+        openModal(fileId);
+    });
+});
