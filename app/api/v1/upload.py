769ce4e65ebdae77f179a59aac7210cd9adb6f52
Maksim <knyazev.maksim2852@gmail.com>
2024-09-16 12:08:52 +0400

    update method

--- app/api/v1/upload.py
+++ app/api/v1/upload.py
@@ -3,16 +3,15 @@ from typing import Annotated
 from app.schemas.user import UserIn, UserOut
 from app.services.user import UserService
 from app.services.file import FileService
-from app.utils.S3 import S3Client, create_s3_client
 import secrets
 from app.services.dependencies import get_user_service, get_file_service
 from sqlalchemy.ext.asyncio import AsyncSession
 from database import get_async_session
 from app.schemas.file import FileSchema
 from datetime import datetime
+from app.utils.S3 import s3_client
+from exceptions import FileTooLargeException
 
-
-s3_client: S3Client = create_s3_client()
 
 upload_router: APIRouter = APIRouter(
     prefix='/upload',
@@ -27,9 +26,10 @@ async def upload_file(
         file: Annotated[UploadFile, File()], 
         user_service: Annotated[UserService, Depends(get_user_service)], 
         file_service: Annotated[FileService, Depends(get_file_service)],
-        session: Annotated[AsyncSession, Depends(get_async_session)]
+        session: Annotated[AsyncSession, Depends(get_async_session)],
     ):
-
+    if file.size > 210000000:
+        raise FileTooLargeException
     ip_address: str = request.client.host
     user: UserOut = await user_service.get_one(ip_address=ip_address, session=session)
 
@@ -43,4 +43,6 @@ async def upload_file(
     
     await file_service.create(session=session, **new_file.model_dump())
     await s3_client.upload_file(file.file, f'{unique_filename}_{file.filename}')
+
+
   
