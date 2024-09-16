7c520bfcecfcdde57b2eed4b74cbd68227709378
Maksim <knyazev.maksim2852@gmail.com>
2024-09-16 12:06:48 +0400

    update s3 methods

--- app/utils/S3.py
+++ app/utils/S3.py
@@ -7,18 +7,21 @@ from config import settings
 class S3Client:
     def __init__(
             self,
-            access_key: str,
-            secret_key: str,
+            aws_access_key_id: str,
+            aws_secret_access_key: str,
             endpoint_url: str,
+            region_name: str,
             bucket_name: str,
     ):
         self.config = {
-            "aws_access_key_id": access_key,
-            "aws_secret_access_key": secret_key,
+            "aws_access_key_id": aws_access_key_id,
+            "aws_secret_access_key": aws_secret_access_key,
             "endpoint_url": endpoint_url,
+            "region_name": region_name
         }
         self.bucket_name = bucket_name
         self.session = get_session()
+
 
     @asynccontextmanager
     async def get_client(self):
@@ -27,35 +30,49 @@ class S3Client:
 
 
 
-    async def upload_file(
-            self,
-            file_path: bytes,
-            file_name: str
-    ):
+    async def upload_file( self, file_path: bytes, file_name: str):
         try:
-
             async with self.get_client() as client:
                 await client.put_object(
                     Bucket=self.bucket_name,
                     Key=file_name,
                     Body=file_path,
                 )
-        except ClientError:
-            raise ClientError
+        except ClientError as e:
+            raise e
+        
+        
+    async def get_file(self, object_key: str, exp: int = 3600):
+        try:
+            async with self.get_client() as client:
+                presigned_url: str = await client.generate_presigned_url(
+                    'get_object', Params={
+                        'Bucket': self.bucket_name, 'Key': object_key, 
+                        'ResponseContentDisposition': f'attachment; filename="{object_key}"'},
+                    ExpiresIn=exp)
+                return presigned_url
+        except ClientError as e:
+            raise e
+
+       
 
     async def delete_file(self, object_name: str):
         try:
             async with self.get_client() as client:
                 await client.delete_object(Bucket=self.bucket_name, Key=object_name)
-        except ClientError:
-            raise ClientError
+        except ClientError as e:
+            raise e
 
 
 def create_s3_client():
     s3_client: S3Client = S3Client(
-        access_key=settings.aws_access_key_id,
-        secret_key=settings.aws_secret_access_key,
+        aws_access_key_id=settings.aws_access_key_id,
+        aws_secret_access_key=settings.aws_secret_access_key,
         endpoint_url=settings.endpoint_url,
-        bucket_name='dropit'
+        bucket_name='dropit',
+        region_name='ru-1'
     )
     return s3_client
+
+
+s3_client: S3Client = create_s3_client()
