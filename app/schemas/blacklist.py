c119c657c738680f7610d8456acfcaae0842fd7b
Maksim <knyazev.maksim2852@gmail.com>
2024-09-16 22:01:03 +0400

    update schema

--- app/schemas/blacklist.py
+++ app/schemas/blacklist.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel, Field, ConfigDict
 
 
 class BlackList(BaseModel):
+    id: int
     ip_address: str
 
     model_config: ConfigDict = ConfigDict(from_attributes=True)
