645b8cbc1295c26a02bdee7975be1ecb7aa2b81e
Maksim <knyazev.maksim2852@gmail.com>
2024-09-16 12:07:59 +0400

    update sql query

--- app/repositories/file.py
+++ app/repositories/file.py
@@ -1,22 +1,29 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.repositories.sqlalchemy import SQLAlchemyRepository
 from app.models.file import File
-from sqlalchemy import select, desc, asc, func
-
+from sqlalchemy import select, desc, func
+from app.models.user import User
 
 class FileRepository(SQLAlchemyRepository):
     model: File = File
 
 
-    async def find_all(self, session: AsyncSession, limit: int = 5, offset: int = 0, **filter_by):
-        stmt = select(
-            self.model.__table__.columns,
-            func.count().over().label('total_count')
-        ).filter_by(**filter_by).order_by(desc(self.model.id)).offset(offset).limit(limit)
+    async def find_all(self, session: AsyncSession, limit: int = 15, offset: int = 0, **filter_by):
+        stmt_data = (
+            select(self.model)
+            .filter_by(**filter_by)
+            .order_by(desc(self.model.id))
+            .offset(offset)
+            .limit(limit)
+        )
+        res_data = await session.execute(stmt_data)
+        user_files = res_data.scalars().all()
 
-        res = await session.execute(stmt)
-        records = res.mappings().all()
-        
-        total_count = records[0]['total_count'] if records else 0
-        
-        return records, total_count
+        stmt_count = (
+            select(func.count())
+            .where(self.model.user_id == User.id)
+        )
+        res_count = await session.execute(stmt_count)
+        total_count = res_count.scalar()
+
+        return user_files, total_count
