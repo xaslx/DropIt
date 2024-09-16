cd9f975094fdc062a6c77c92c19e965e1246b710
Maksim <knyazev.maksim2852@gmail.com>
2024-09-16 17:51:33 +0400

    add lifespan, and router for tg

--- main.py
+++ main.py
@@ -2,12 +2,27 @@ from fastapi import FastAPI
 from app.api.v1.main import main_router
 from app.api.v1.upload import upload_router
 from fastapi.staticfiles import StaticFiles
+from app.bot.run import on_startup
+from logger import logger
+from contextlib import asynccontextmanager
+from config import settings
+from app.bot.run import handle_web_hook
 
 
-app: FastAPI = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+
+    await on_startup()
+    logger.info("Fastapi приложение и Бот запущены")
+    yield
+
+
+app: FastAPI = FastAPI(lifespan=lifespan)
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 
 app.include_router(main_router)
 app.include_router(upload_router)
+app.add_route(f"/{settings.bot_token_tg}", handle_web_hook, methods=["POST"])
