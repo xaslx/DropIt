37f5460fc5458caf1b7f76882bf2ba6faa8355b3
Maksim <knyazev.maksim2852@gmail.com>
2024-09-16 12:42:39 +0400

    add new dependencies

--- app/services/dependencies.py
+++ app/services/dependencies.py
@@ -4,6 +4,8 @@ from app.services.user import UserService
 from app.repositories.file import FileRepository
 from app.services.file import FileService
 
+from app.repositories.blacklist import BlackListRepository
+from app.services.blacklist import BlackListService
 
 def get_user_service():
     return UserService(UserRepository)
@@ -11,3 +13,7 @@ def get_user_service():
 
 def get_file_service():
     return FileService(FileRepository)
+
+
+def get_blacklist_service():
+    return BlackListService(BlackListRepository)
