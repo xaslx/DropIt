930d67091b6139846f03f182b0de07c125533338
Maksim <knyazev.maksim2852@gmail.com>
2024-09-20 19:23:26 +0400

    add logging

--- app/repositories/sqlalchemy.py
+++ app/repositories/sqlalchemy.py
@@ -1,7 +1,8 @@
 from app.repositories.base import AbstractRepository
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import insert, update, delete, select
-
+from logger import logger
+from sqlalchemy.exc import SQLAlchemyError
 
 
 class SQLAlchemyRepository(AbstractRepository):
@@ -9,22 +10,39 @@ class SQLAlchemyRepository(AbstractRepository):
     model = None
 
     async def add(self, session: AsyncSession, **data: dict):
-        stmt = insert(self.model).values(**data).returning(self.model.__table__.columns)
-        res = await session.execute(stmt)
-        await session.commit()
-        return res.mappings().one_or_none()
+        try:
+            stmt = insert(self.model).values(**data).returning(self.model.__table__.columns)
+            res = await session.execute(stmt)
+            await session.commit()
+            return res.mappings().one_or_none()
+        except (SQLAlchemyError, Exception) as e:
+            logger.error(
+                'Ошибка при добавлении записи в базу данных', extra={'данные': data, 'ошибка': e}
+            )
+
+
 
     async def find_one_or_none(self, session: AsyncSession, **filter_by):
-        stmt = select(self.model.__table__.columns).filter_by(**filter_by)
-        res = await session.execute(stmt)
-        return res.mappings().one_or_none()
+        try:
+            stmt = select(self.model).filter_by(**filter_by)
+            res = await session.execute(stmt)
+            return res.scalar_one_or_none()
+        except (SQLAlchemyError, Exception) as e:
+            logger.error(f'Ошибка при поиске значения в базе данных', extra={'ошибка': e})
+
 
     async def find_all(self, session: AsyncSession, **filter_by):
-        stmt = select(self.model.__table__.columns).filter_by(**filter_by)
-        res = await session.execute(stmt)
-        return res.mappings().all()
+        try:
+            stmt = select(self.model).filter_by(**filter_by)
+            res = await session.execute(stmt)
+            return res.scalar_one_or_none()
+        except (SQLAlchemyError, Exception) as e:
+            logger.error(f'Ошибка при поиске всех значений в базе данных', extra={'ошибка': e})
 
     async def delete(self, session: AsyncSession, id: int) -> int:
-        stmt = delete(self.model).filter_by(id=id).returning(self.model.id)
-        await session.execute(stmt)
-        await session.commit()
+        try:
+            stmt = delete(self.model).filter_by(id=id).returning(self.model.id)
+            await session.execute(stmt)
+            await session.commit()
+        except (SQLAlchemyError, Exception) as e:
+            logger.error(f'Ошибка при удалении значения из базы данных', extra={'ошибка': e})
