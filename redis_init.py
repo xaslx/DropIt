6cf35db4669f876ba92f772d594c5b764d49d712
Maksim <knyazev.maksim2852@gmail.com>
2024-09-20 19:20:30 +0400

    add redis

---
+++ redis_init.py
@@ -0,0 +1,10 @@
+from redis.asyncio import Redis as aioredis
+from redis import Redis
+
+from config import settings
+
+redis: Redis = aioredis.from_url(
+    f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}",
+    encoding="utf-8",
+    decode_responses=True,
+)
