56ce25e2194537ffdf3cfd8617b88d433b6827db
Maksim <knyazev.maksim2852@gmail.com>
2024-09-20 19:23:12 +0400

    update query

--- app/repositories/file.py
+++ app/repositories/file.py
@@ -3,6 +3,7 @@ from app.repositories.sqlalchemy import SQLAlchemyRepository
 from app.models.file import File
 from sqlalchemy import select, desc, func
 from app.models.user import User
+from app.schemas.file import FileSchemaOut
 
 class FileRepository(SQLAlchemyRepository):
     model: File = File
@@ -10,20 +11,20 @@ class FileRepository(SQLAlchemyRepository):
 
     async def find_all(self, session: AsyncSession, user_id: int, limit: int = 15, offset: int = 0):
         stmt_data = (
-            select(self.model)
+            select(self.model.__table__.columns)
             .where(self.model.user_id == user_id)
             .order_by(desc(self.model.id))
             .offset(offset)
             .limit(limit)
         )
         res_data = await session.execute(stmt_data)
-        user_files = res_data.scalars().all()
+        user_files: FileSchemaOut = res_data.mappings().all()
 
         stmt_count = (
             select(func.count())
             .where(self.model.user_id == user_id)
         )
         res_count = await session.execute(stmt_count)
-        total_count = res_count.scalar()
+        total_count: int = res_count.scalar()
 
         return user_files, total_count
