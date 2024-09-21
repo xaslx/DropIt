c80ad969af0a3390b72b491b7c8367dfc09320a6
Maksim <knyazev.maksim2852@gmail.com>
2024-09-21 18:56:23 +0400

    add cache

--- app/utils/templating.py
+++ app/utils/templating.py
@@ -1,6 +1,8 @@
 from fastapi.templating import Jinja2Templates
+from functools import lru_cache
 
 
-async def get_template() -> Jinja2Templates:
+@lru_cache()
+def get_template() -> Jinja2Templates:
     templates: Jinja2Templates = Jinja2Templates(directory="app/static/templates")
     return templates
