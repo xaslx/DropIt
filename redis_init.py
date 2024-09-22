ab76756e8c7a6b2226b3478df1f832298cece32a
Maksim <knyazev.maksim2852@gmail.com>
2024-09-22 18:08:54 +0400

    check connection redis

--- redis_init.py
+++ redis_init.py
@@ -1,26 +1,26 @@
+import asyncio
 from redis.asyncio import Redis as aioredis
-from redis import Redis
 from redis.exceptions import RedisError
 from config import settings
 from logger import logger
 
-
-# redis: Redis = aioredis.from_url(
-#     f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}",
-#     encoding="utf-8",
-#     decode_responses=True,
-# )
-
-redis = None
-async def init_redis() -> aioredis | None:
+async def check_redis_connection(redis: aioredis) -> bool:
     try:
-        return aioredis.from_url(
-            f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}",
-            encoding="utf-8",
-            decode_responses=True,
-            socket_timeout=1,
-            socket_connect_timeout=1,
-        )
+        await redis.ping()
+        return True
     except RedisError:
+        return False
+
+async def init_redis() -> aioredis | None:
+    redis = aioredis.from_url(
+        f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}",
+        encoding="utf-8",
+        decode_responses=True,
+        socket_timeout=1,
+        socket_connect_timeout=1,
+    )
+    if await check_redis_connection(redis):
+        return redis
+    else:
         logger.error('Ошибка подключения к Redis')
         return None
