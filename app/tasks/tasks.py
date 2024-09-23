51350bc582f2a20f10771c888ca82decfcbd8f13
Maksim <knyazev.maksim2852@gmail.com>
2024-09-23 10:09:03 +0400

    update tasks

--- app/tasks/tasks.py
+++ app/tasks/tasks.py
@@ -14,33 +14,30 @@ async def delete_file_from_s3(filename: str):
 
 
 async def send_report(file_id: int, file_url: str):
-    for user in settings.id_admins_telegram:
-        await bot.send_message(
-            chat_id=user,
-            text=
-            '❗️ #репорт\n\n'
-            f'file_id: {file_id}\n\n'
-            f'file_url: {file_url}'
-        )
+    await bot.send_message(
+        chat_id=settings.id_admins_telegram,
+        text=
+        '❗️ #репорт\n\n'
+        f'file_id: {file_id}\n\n'
+        f'file_url: {file_url}'
+    )
 
 
 async def add_new_file(file_id: int, file_url: str):
-    for user in settings.id_admins_telegram:
-        await bot.send_message(
-            chat_id=user,
-            text=
-            '✅ #новый_файл\n\n'
-            f'Добавлен новый файл\n\n'
-            f'file_id: {file_id}\n\n'
-            f'file_url: {file_url}'
-        )
+    await bot.send_message(
+        chat_id=settings.id_admins_telegram,
+        text=
+        '✅ #новый_файл\n\n'
+        f'Добавлен новый файл\n\n'
+        f'file_id: {file_id}\n\n'
+        f'file_url: {file_url}'
+    )
 
 
 async def delete_file_admin(file_id: int):
-    for user in settings.id_admins_telegram:
-        await bot.send_message(
-            chat_id=user,
-            text=
-            f'❌ #файл_удален\n\n'
-            f'Файл {file_id}, был удален администратором'
-        )
+    await bot.send_message(
+        chat_id=settings.id_admins_telegram,
+        text=
+        f'❌ #файл_удален\n\n'
+        f'Файл {file_id}, был удален администратором'
+    )
