6bdee0fff02ba35e8ebc1822fe228d39c409ffff
Maksim <knyazev.maksim2852@gmail.com>
2024-11-09 18:08:54 +0400

    update user model

--- app/models/user.py
+++ app/models/user.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import TYPE_CHECKING
 
 
-
 if TYPE_CHECKING:
     from app.models.file import File
 
@@ -13,6 +12,6 @@ class User(Base):
     __tablename__ = 'users'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    ip_address: Mapped[str] = mapped_column(unique=True)
+    cookie_uuid: Mapped[str] = mapped_column(unique=True)
     
     files: Mapped[list['File']] = relationship(back_populates='user')
